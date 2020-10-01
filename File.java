44cbe87e299598f4624b14b4467bd31b6ceea30e
Sam Becerra <samuel@grupoconsultic.com.mx>
2020-10-01 15:50:29 -0500

    Sum Function

--- File.java
+++ File.java
@@ -2,9 +2,17 @@ package UTR;
 
 public class File {
 
+    static int sum(int x, int y){
+        
+        return x+y;
+
+    }
+    
     public static void main(String[] args) {
-        int x = 0;
 
         System.out.println("Hello world! ");
+
+        System.out.println("Sum of 2 numbers: " + sum(10, 5));
+
     }
 }
