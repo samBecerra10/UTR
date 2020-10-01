1ef21b8ec1461c19e00fd5e9f94cc4a9ad7dcfa4
Sam Becerra <samuel@grupoconsultic.com.mx>
2020-10-01 15:20:40 -0500

    Update File.java
    New variable  x=0

--- File.java
+++ File.java
@@ -3,6 +3,9 @@ package UTR;
 public class File {
 
     public static void main(String[] args) {
+        int x = 0;
+
         System.out.println("Hello world! ");
+        System.out.println("Num x: " + x);
     }
 }
